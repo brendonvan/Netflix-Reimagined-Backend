c2213094204a1bd6a6405bbb788911fb072b227f
SeanBu <seanbuchas@gmail.com>
2022-09-15 22:23:53 -0400

    started auth

---
+++ controllers/authController.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const { User } = require("../models");
+const { send } = require("process");
+
+router.use(express.json());
+router.use(express.urlencoded({ extended: false }));
+
+// login
+router.post('/login', async function (req, res, next) {
+
+    try {
+        const foundUser = await User.findOne({ username: req.body.username });
+
+        if (!foundUser) {
+            return res.redirect('/home');
+        } else {
+            const match = await bcrypt.compare(req.body.password, foundUser.password);
+            if (!match) return res.send("invalid password");
+            req.session.currentUser = {
+                id: foundUser._id,
+                username: foundUser.username
+            };
+        }
+    } catch (err) {
+        console.log(err);
+        next();
+    }
+
+});
+
+// register
+router.post("/register", async (res, res) => {
+    try {
+        const foundUser = await User.exists({ username: req.body.username });
+        if (foundUser) {
+            res.redirect('/login');
+        } else {
+            const salt = await bcrypt.genSalt(14);
+            const hash = await bcrypt.hash(req.body.password, salt);
+
+            req.body.password = hash;
+
+            const newUser = await User.create(req.body);
+            console.log(`New User: ${newUser}`);
+        }
+    } catch (err) {
+        console.log(err);
+        next();
+    }
+});
+
+// logout
+router.get("/logout", async (req, res) => {
+    try {
+        await req.session.destroy();
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,9 +4,13 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const methodOverride = require('method-override');
+const session = require('express-session');
+const MongoStore = require("connect-mongo");
 
 const PORT = 4000;
 const mainController = require('./controllers/mainController');
+const authController = require('./controllers/authController')
 
 const app = express();
 
@@ -14,7 +18,18 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
+app.use(session)({
+    store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
+    secret: '258369147',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 2
+    }
+})
+
 app.use('/movie', mainController);
+app.use('/auth', authController);
 
 app.get("/", (req, res) => {
     res.send("Hello Netflicks");
